b498cecfd1946d95d8f2e9a39b8f4f0e819c5e7c
jfrey <nomhad@gmail.com>
2016-08-02 21:11:16 +0200

    robust to deco

--- lsl2joy_continuous.py
+++ lsl2joy_continuous.py
@@ -1,14 +1,17 @@
 
 from pylsl import StreamInlet, resolve_stream
-import uinput
+import uinput, time
 
 # map output to angles, continuous
 
+# how many seconds without samples should we wait before we timeout?
+LSL_DECO_TIMEOUT=2
+
+# we are not doing blocking call for LSL, but we don't want to use too much CPU either. Warning: if lower than input frequency, missed samples and/or shift will occur.
+LSL_MAX_FREQ=1024
+
 # stream name of classifier output in openvibe
 stream_name = "hyperplane"
-
-# rough prototype, we'll use a simple threshold for now
-threshold = 0.3
 
 # one virtual joystick
 events = (
@@ -21,20 +24,50 @@ device = uinput.Device(events)
 # center
 device.emit(uinput.ABS_X, 128)
 device.emit(uinput.ABS_Y, 128)
-  
-# first resolve an EEG stream on the lab network
-print("looking for LSL stream named [" + stream_name +"]")
-streams = resolve_stream('name', stream_name)
 
-# create a new inlet to read from the stream
-inlet = StreamInlet(streams[0])
+inlet = None
 
-print("got it")
+# used to (re)connect to LSL
+def lookout_stream():
+    # first resolve an EEG stream on the lab network
+    print("looking for LSL stream named [" + stream_name +"]")
+    streams = resolve_stream('name', stream_name)
+
+    # create a new inlet to read from the stream
+    print("got it")
+    return StreamInlet(streams[0])  
 
 while True:
+    # connect / reco. NB: blocking call until one stream is found
+    if inlet == None:
+        inlet = lookout_stream()
+        # init lsl for timeout detection
+        last_sample = time.time()
+        
     # get a new sample
-    sample, timestamp = inlet.pull_sample()
-    print(timestamp, sample)
+    try:
+        sample, timestamp = inlet.pull_sample(0)
+       
+    except:
+        print "disconnected"
+        inlet = None
+        continue
+    else:
+        # check for time out and loop over if no sample
+        if not sample:
+            # let stream go and loop over if reached timeout
+            time_since_last_sample = time.time() - last_sample
+            if time_since_last_sample > LSL_DECO_TIMEOUT:
+                print "time out"
+                inlet = None
+            # loop no matter what
+            continue
+        else:
+            last_sample = time.time()
+            print(timestamp, sample)
+        
+
+    
     # retrieve first channel
     value = sample[0]
     # convert to angle, from -1,1 to 0, 255
